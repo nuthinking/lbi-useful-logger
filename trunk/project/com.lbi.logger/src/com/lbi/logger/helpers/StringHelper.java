b6d30f588fe2698c0115c88dcd457768e3771658
nuthinking <nuthinking@5aece955-7939-0410-be9c-cd5e9b26f3ef>
2008-08-16 10:51:49 +0000

    Modified a couple of settings and exported as version 1.0.5. The fatal style seems fixed on the Mac.

--- trunk/project/com.lbi.logger/src/com/lbi/logger/helpers/StringHelper.java
+++ trunk/project/com.lbi.logger/src/com/lbi/logger/helpers/StringHelper.java
@@ -6,12 +6,15 @@ public class StringHelper
 	{
 		// TODO Auto-generated method stub
 		int start_pos = command.indexOf(attribute_name);
-		if(start_pos == -1) return null;
+		if(start_pos == -1)
+			return null;
 		start_pos = command.indexOf("=",start_pos);
-		if(start_pos == -1) return null;
+		if(start_pos == -1)
+			return null;
 		start_pos++;
 		int end_pos = command.indexOf(" ", start_pos);
-		if(end_pos == -1) end_pos = command.length();
+		if(end_pos == -1)
+			end_pos = command.length();
 		return command.substring(start_pos, end_pos);
 	}
 }
